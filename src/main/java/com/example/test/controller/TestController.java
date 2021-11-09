9e5f13ac333c3b5be65cd7da77513bcef11f7c14
201402407 <banner4@naver.com>
2021-11-09 18:26:26 +0900

    POST, GET ResponseEntity 생성 및 API 샘플 제작

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -3,7 +3,9 @@ package com.example.test.controller;
 import java.util.Optional;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.example.test.vo.SameWordVO;
 import ch.qos.logback.classic.Logger;
 import common.CommonResponse;
 import common.CustomResponse;
+import common.ErrorResponse;
 
 // @Controller + @ResponseBody
 @RestController
@@ -39,21 +42,50 @@ public class TestController {
 		return value;
 	}
 	
-	@RequestMapping(value = "/checkSameWord", method = RequestMethod.POST)
-	public ResponseEntity<? extends CustomResponse> checkSameWord(@RequestBody SameWordVO pvo) {
+	@RequestMapping(value = "/checkSameWord/{word}", method = RequestMethod.GET)
+	public ResponseEntity<? extends CustomResponse> checkSameWordGet(@PathVariable("word") String word) {
 		String answerWord = "TEST";
+		final ResponseEntity responseEntity;
 		
-		Optional.ofNullable(pvo.getWord()).ifPresent(word -> {
+		// 값 존재
+		if(Optional.ofNullable(word).isPresent()) {
 			String resultMsg = word.equals(answerWord) ? "정답입니다!" : "틀렸습니다ㅠㅠ";
 			boolean isSame = word.equals(answerWord);
 			String resultWord = answerWord;
+			
 			SameWordRVO rvo = SameWordRVO.builder()
 					.resultMsg(resultMsg)
 					.isSame(isSame)
 					.resultWord(resultWord)
 					.build();
 			
-			return ResponseEntity.ok().body(new CommonResponse<SameWordRVO>(rvo));
-		});
+			return ResponseEntity.ok().body(new CommonResponse<SameWordRVO>(rvo));	
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("입력 값 오류 발생", HttpStatus.BAD_REQUEST.value()));
+	}
+	
+	@RequestMapping(value = "/checkSameWord", method = RequestMethod.POST)
+	public ResponseEntity<? extends CustomResponse> checkSameWord(@RequestBody SameWordVO pvo) {
+		String answerWord = "TEST";
+		final ResponseEntity responseEntity;
+		
+		// 값 존재
+		if(Optional.ofNullable(pvo.getWord()).isPresent()) {
+			String word = pvo.getWord();
+			String resultMsg = word.equals(answerWord) ? "정답입니다!" : "틀렸습니다ㅠㅠ";
+			boolean isSame = word.equals(answerWord);
+			String resultWord = answerWord;
+			
+			SameWordRVO rvo = SameWordRVO.builder()
+					.resultMsg(resultMsg)
+					.isSame(isSame)
+					.resultWord(resultWord)
+					.build();
+			
+			return ResponseEntity.ok().body(new CommonResponse<SameWordRVO>(rvo));	
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("입력 값 오류 발생", HttpStatus.BAD_REQUEST.value()));
 	}
 }
